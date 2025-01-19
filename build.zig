b0c7ea11b6f3e2197ba3ab422417e90a725545f8
spineda2019 <spineda.wpi.alum@gmail.com>
2025-01-19 11:23:03 -0500

    fix build script for proper emulation, program counter, and stack pointer

--- build.zig
+++ build.zig
@@ -4,31 +4,6 @@ const Error: type = error{
     UnsupportedTarget,
 };
 
-fn build_riscv32(
-    b: *std.Build,
-    optimize: *const std.builtin.OptimizeMode,
-) !*std.Build.Step.Compile {
-    const exe = b.addExecutable(.{
-        .name = "osOS.elf",
-        .target = b.resolveTargetQuery(.{
-            .cpu_arch = .riscv32,
-            .os_tag = .freestanding,
-            .abi = .none,
-        }),
-        .root_source_file = b.path("architecture/riscv32/kernel.zig"),
-        .optimize = optimize.*,
-        .strip = false,
-    });
-
-    exe.entry = .disabled;
-
-    exe.setLinkerScript(b.path("architecture/riscv32/link.ld"));
-
-    b.installArtifact(exe);
-
-    return exe;
-}
-
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
@@ -39,11 +14,24 @@ pub fn build(b: *std.Build) void {
     // for restricting supported target set are available.
     // const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
-    const exe = try build_riscv32(b, &optimize);
+    const exe = b.addExecutable(.{
+        .name = "osOS.elf",
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .riscv32,
+            .os_tag = .freestanding,
+            .abi = .none,
+        }),
+        .root_source_file = b.path("architecture/riscv32/kernel.zig"),
+        .optimize = .ReleaseSmall,
+        .strip = false,
+    });
+
+    exe.entry = .disabled;
+
+    exe.setLinkerScript(b.path("architecture/riscv32/link.ld"));
+
+    b.installArtifact(exe);
+
     const run = b.addSystemCommand(&.{
         "qemu-system-riscv32",
         "-machine",
