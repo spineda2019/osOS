6c4ea86a770baf5eee1ee22c0d0733200c8371a8
spineda2019 <spineda.wpi.alum@gmail.com>
2025-01-21 18:53:37 -0500

    add iso steps for x86 to build.zig

--- architecture/x86/kernel.zig
+++ architecture/x86/kernel.zig
@@ -1,4 +1,9 @@
-export fn boot() linksection(".text") callconv(.Naked) noreturn {
+/// Entry point for the x86 kernel. Stack must be set up
+export fn boot() align(4) linksection(".text") callconv(.Naked) noreturn {
+    asm volatile (
+        \\.intel_syntax noprefix
+        \\mov EAX, 0x4242
+    );
     while (true) {
         asm volatile ("");
     }
--- build.zig
+++ build.zig
@@ -8,10 +8,12 @@ const Error: type = error{
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
+    const kernel_name = "osOS.elf";
     //**************************************************************************
-    // RISCV-32 Setup
+    //                              RISCV-32 Setup                             *
+    //**************************************************************************
     const exe = b.addExecutable(.{
-        .name = "osOS.elf",
+        .name = kernel_name,
         .target = b.resolveTargetQuery(.{
             .cpu_arch = .riscv32,
             .os_tag = .freestanding,
@@ -36,6 +38,7 @@ pub fn build(b: *std.Build) void {
 
     riscv32_step.dependOn(&out.step);
 
+    // this makes the install target build everything
     b.getInstallStep().dependOn(&out.step);
 
     const run = b.addSystemCommand(&.{
@@ -59,4 +62,68 @@ pub fn build(b: *std.Build) void {
 
     run_step.dependOn(&run.step);
     //**************************************************************************
+    //                                 x86 Setup                               *
+    //**************************************************************************
+    const x86_exe = b.addExecutable(.{
+        .name = kernel_name,
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .x86,
+            .os_tag = .freestanding,
+            .abi = .none,
+        }),
+        .root_source_file = b.path("architecture/x86/kernel.zig"),
+        .optimize = .ReleaseSmall,
+        .strip = false,
+    });
+    x86_exe.entry = .disabled;
+    x86_exe.setLinkerScript(b.path("architecture/x86/link.ld"));
+
+    const x86_step = b.step("x86", "Build the x86 Kernel");
+
+    const x86_out = b.addInstallArtifact(x86_exe, .{
+        .dest_dir = .{
+            .override = .{
+                .custom = "x86",
+            },
+        },
+    });
+
+    x86_step.dependOn(&x86_out.step);
+
+    // this makes the install target build everything
+    b.getInstallStep().dependOn(&x86_out.step);
+
+    // TODO: Make these copy steps system agnostic
+    const create_x86_iso_structure = b.addSystemCommand(&.{
+        "mkdir",
+        "-p",
+        "zig-out/x86/iso/boot/grub",
+    });
+    create_x86_iso_structure.step.dependOn(x86_step);
+
+    const copy_grub_files = b.addSystemCommand(&.{
+        "cp",
+        "architecture/x86/stage2_eltorito",
+        "zig-out/x86/iso/boot/grub",
+    });
+
+    copy_grub_files.step.dependOn(&create_x86_iso_structure.step);
+
+    const copy_kernel = b.addSystemCommand(&.{
+        "cp",
+    });
+    copy_kernel.addArtifactArg(x86_exe);
+    copy_kernel.addArg("zig-out/x86/iso/boot");
+    copy_kernel.step.dependOn(&copy_grub_files.step);
+
+    const x86_run = b.addSystemCommand(&.{
+        "echo",
+        "TODO: x86 run step",
+    });
+
+    x86_run.step.dependOn(&copy_kernel.step);
+
+    const x86_run_step = b.step("run_x86", "Boot kernel with qemu on x86");
+
+    x86_run_step.dependOn(&x86_run.step);
 }
