7654f8d4432d4a673dc739b71bfb67c0c4420eaa
spineda2019 <spineda.wpi.alum@gmail.com>
2025-01-25 13:20:10 -0500

    add x86 external entry assembly file

---
+++ architecture/x86/entry.s
@@ -0,0 +1,21 @@
+global boot
+extern kmain
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS        equ 0x0
+CHECKSUM     equ -MAGIC_NUMBER
+KERNEL_STACK_SIZE equ 4096
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE                     ; Setup stack for external code
+
+section .text
+align 4
+    dd MAGIC_NUMBER
+    dd FLAGS
+    dd CHECKSUM
+    mov esp, kernel_stack + KERNEL_STACK_SIZE  ; setup stack pointer to end of kernel_stack
+
+boot:
+    jmp kmain
