100f10426ea192e7e4c046c6d816f061a3a5ece9
spineda2019 <spineda.wpi.alum@gmail.com>
2025-01-28 19:00:16 -0500

    stub type checking for intToStr

---
+++ architecture/common/format.zig
@@ -0,0 +1,22 @@
+pub fn intToString(comptime int_type: type, number: int_type) []const u8 {
+    if (comptime @typeInfo(int_type) != .int) {
+        @compileError("Error: expected an integer type, found: " ++ @typeName(int_type));
+    }
+
+    const digit_count = comptime @floor(@bitSizeOf(int_type) * 0.30103) + 1;
+    var remainder: int_type = number;
+    var buffer: [digit_count]u8 = comptime .{0} ** digit_count;
+    var ptr = buffer.len - 1;
+    while (remainder > 0) : ({
+        if (ptr > 0) {
+            ptr -= 1;
+        }
+        remainder /= 10;
+    }) {
+        // int cast to u8 should be safe. Modulo will be 9 max.
+        const digit: u8 = remainder % 10;
+        buffer[ptr] = digit + 48;
+    }
+
+    return buffer[ptr..];
+}
