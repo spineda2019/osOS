dacdeb3d9a69bf61ffab5afcddda7f237ec7d97e
spineda2019 <spineda.wpi.alum@gmail.com>
2025-01-28 22:09:28 -0500

    Update all relavent files to notice of GPLv3

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Oso's Os, or BearOS
 If an update to zig breaks (one of) the loader, the bundled llvm assembler
 may be placing symbols at wonky addresses. Use the readelf tool to ensure
 symbols are loaded where you would expect
+
+## License
+Not including the GPLv3 compatible licensed binaries packaged with osOS that I
+have not built, the osOS kernel in its entirety is licensed under the GPLv3.
+See the LICENSE file for information.
+
+## License Notice
+Copyright (C) 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
+
--- architecture/x86/entry.zig
+++ architecture/x86/entry.zig
@@ -1,3 +1,19 @@
+// boot.zig - Entry point for the osOS kernel on 32-bit x86
+// Copyright (C) 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 export fn boot() align(4) linksection(".text") callconv(.Naked) noreturn {
     asm volatile (
         \\.equ MAGIC_NUMBER,      0x1BADB002
