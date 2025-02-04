728ae8120176ddc15f8646b0f8e908f9a5bc21a2
spineda2019 <spineda.wpi.alum@gmail.com>
2025-02-03 22:37:42 -0500

    make license notice a doc comment

--- format/format.zig
+++ format/format.zig
@@ -1,19 +1,20 @@
 // format.zig - Architecture agnostic API for numeric formatting
-// Copyright (C) 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//! Copyright (C) 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+/// Convert an integer to a string representation at compile time
 pub fn StringFromInt(
     comptime array_size: comptime_int,
 ) type {
