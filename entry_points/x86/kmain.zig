e1aba77eac7796f91a86bb1bf0cc5eda944cf96c
spineda2019 <spineda.wpi.alum@gmail.com>
2025-02-16 14:33:54 -0500

    remove arch_api entirely

--- entry_points/x86/kmain.zig
+++ entry_points/x86/kmain.zig
@@ -16,8 +16,9 @@
 
 //! This module contains logic for the setup and entry of the x86 kernel
 
-const framebuffer_api = @import("framebuffer.zig");
+const framebuffer_api = @import("framebuffer/framebuffer.zig");
 
+/// Actual root "main" function of the x86 kernel. Jumped to from entry point
 pub fn kmain() noreturn {
     framebuffer_api.FrameBuffer.clear();
     framebuffer_api.FrameBuffer.printWelcomScreen();
