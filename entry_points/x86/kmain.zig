3b3da096bf0502b4172bec6be912a25d52194ada
spineda2019 <spineda.wpi.alum@gmail.com>
2025-02-16 15:42:07 -0500

    change doc comment

--- entry_points/x86/kmain.zig
+++ entry_points/x86/kmain.zig
@@ -1,20 +1,18 @@
-// kmain.zig - The central core of osOS on x86; where the boot routine jumps to
-// Copyright (C) 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-//! This module contains logic for the setup and entry of the x86 kernel
+//! kmain.zig - The central core of osOS on x86; where the boot routine jumps to
+//! Copyright (C) 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const framebuffer_api = @import("framebuffer/framebuffer.zig");
 
