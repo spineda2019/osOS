311c955d1e587a4f4fa81ccf0a981ab9ab06456f
spineda2019 <spineda.wpi.alum@gmail.com>
2025-02-24 21:30:19 -0500

    simplify writer interface

--- format/print.zig
+++ format/print.zig
@@ -16,69 +16,62 @@
 
 /// Kernal wide interface for storing the arch dependent implementation details
 /// of writing to the screen.
-pub const KernelWriter = struct {
-    /// pointer to the ACTUAL struct/implementation doing the writing.
-    instance: *anyopaque,
-
-    /// Table storing mandatory methods of the "instance" member a KernelWriter
-    /// must have. The pillar of making a KernelWriter a quasi-interface.
-    vtable: *const VTable,
-
-    pub const VTable = struct {
-        /// Write a raw string
-        writeRaw: *const fn (self: *anyopaque, raw_string: []const u8) void,
-
-        /// Write a human readable version of a value
-        writeValue: *const fn (self: *anyopaque, value: anytype) void,
-
-        /// Empty and write the interna; buffer. This should reset some internal
-        /// buffer sentinel
-        flush: *const fn (self: *anyopaque) void,
-
-        isBufferFull: *const fn (self: *anyopaque) bool,
-
-        /// Set the current position in the buffer to a specific char. This
-        /// should increment some internal buffer sentinel.
-        setCurrentChar: *const fn (self: *anyopaque, char: u8) void,
-    };
+pub const VTable = struct {
+    /// Write a string
+    write: *const fn (raw_string: []const u8) void,
 };
 
-pub fn printf(
+/// Classic C-style printf for the kernel
+pub fn kprintf(
     comptime format_string: []const u8,
     args: anytype,
-    writer: KernelWriter,
+    writer: VTable,
 ) void {
-    defer writer.vtable.flush(writer.instance);
-
-    var flag: bool = false;
+    var buffer: [32]u8 = .{0} ** 32;
+    var internal_sentinel: u8 = 0;
     var arg_sentinel: u8 = 0;
 
+    defer {
+        // flush
+        writer.write(buffer[0..internal_sentinel]);
+    }
+
+    var flag: bool = false;
     for (format_string) |letter| {
-        if (writer.vtable.isBufferFull(writer.instance)) {
-            writer.vtable.flush(writer.instance);
+        if (internal_sentinel == buffer.len) {
+            // flush and reset ptr
+            writer.write(&buffer);
+            internal_sentinel = 0;
         }
 
         switch (letter) {
             '%' => {
                 if (flag) {
                     // write only the single % literal for '%%'
-                    writer.vtable.setCurrentChar(writer.instance, '%');
+                    buffer[internal_sentinel] = '%';
+                    internal_sentinel += 1;
                 }
+
                 flag = !flag;
             },
             else => {
                 if (flag) {
-                    writer.vtable.writeValue(inline for (args, 0..) |arg, i| {
+                    buffer[internal_sentinel] = 'X';
+                    internal_sentinel += 1;
+
+                    // TODO: take this arg and fmt print it
+                    inline for (args, 0..) |arg, i| {
                         if (i == arg_sentinel) {
                             arg_sentinel += 1;
                             break arg;
                         }
-
                         break null;
-                    });
+                    }
+
                     flag = false;
                 } else {
-                    writer.vtable.setCurrentChar(writer.instance, letter);
+                    buffer[internal_sentinel] = letter;
+                    internal_sentinel += 1;
                 }
             },
         }
