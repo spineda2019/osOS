b6e14ea0474722a1719870607edbf9440680afda
spineda2019 <spineda.wpi.alum@gmail.com>
2025-03-15 01:03:51 -0400

    cleanup

--- entry_points/x86/entry.zig
+++ entry_points/x86/entry.zig
@@ -20,12 +20,11 @@ const kmain = @import("kmain.zig").kmain;
 /// defined in bss.
 const stack_end: [*]u8 = @extern([*]u8, .{ .name = "__stack_end" });
 
-export fn boot() align(4) linksection(".text") callconv(.Naked) noreturn {
+export fn boot() align(4) linksection(".text") callconv(.naked) noreturn {
     asm volatile (
         \\.equ MAGIC_NUMBER,      0x1BADB002
         \\.equ FLAGS,             0x0
         \\.equ CHECKSUM,          -MAGIC_NUMBER
-        \\.equ KERNEL_STACK_SIZE, 4096
         \\
         \\    .long MAGIC_NUMBER
         \\    .long FLAGS
