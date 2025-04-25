56079945837f2a484e2e9e4cd55fd0a0ccf24d70
spineda2019 <spineda.wpi.alum@gmail.com>
2025-04-25 17:29:25 -0400

    process typo fix

--- process/process.zig
+++ process/process.zig
@@ -13,9 +13,6 @@
 //!
 //! You should have received a copy of the GNU General Public License
 //! along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//!
-//! This module provides the entry point of the kernel on RISC-V 32 bit systems
-//! Specifically, this is currently designed for the QEMU "virt" machine
 
 const MAX_PROCESS_COUNT: comptime_int = 8;
 
