742c30729053a82caf3f6f621ddae6e8aeb3fd46
spineda2019 <spineda.wpi.alum@gmail.com>
2025-04-25 17:49:19 -0400

    add more pic initialization logic

--- entry_points/x86/interrupts/pic.zig
+++ entry_points/x86/interrupts/pic.zig
@@ -15,6 +15,7 @@
 //! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const as = @import("x86asm");
+const serial = @import("x86serial");
 
 pub const PIC = struct {
     const master_command_port: u16 = 0x0020;
@@ -22,8 +23,17 @@ pub const PIC = struct {
     const slave_command_port: u16 = 0x00A0;
     const slave_data_port: u16 = 0x00A1;
 
-    const pic_acknowledge: u8 = 0x20;
-    const pic_initialize: u8 = 0x11;
+    pub const common_messages = struct {
+        const pic_acknowledge: u8 = 0x20;
+        /// A "raw" initialization byte is technically 0x10, but ORing this with 0x1
+        /// indicates we ae using "Cascading Mode", which gives us access to
+        /// multiple chained PICs, so we'll just use 0x11 by default.
+        const pic_initialize: u8 = 0x11;
+
+        const initialize_8086_mode: u8 = 0x01;
+
+        const unmask: u8 = 0;
+    };
 
     /// Send acknowledgement to the PIC chip that sent an interrupt request. If
     /// The slave sends a request, both the slave and master need to be
@@ -31,14 +41,62 @@ pub const PIC = struct {
     pub fn sendAcknowledgement(interrupt_request: u8) void {
         if (interrupt_request >= 8) {
             // must also send ack to the slave chip
-            as.assembly_wrappers.x86_out(slave_command_port, pic_acknowledge);
+            as.assembly_wrappers.x86_out(
+                slave_command_port,
+                common_messages.pic_acknowledge,
+            );
         }
 
-        as.assembly_wrappers.x86_out(master_command_port, pic_acknowledge);
+        as.assembly_wrappers.x86_out(
+            master_command_port,
+            common_messages.pic_acknowledge,
+        );
     }
 
-    pub fn remap(master_offset: u32, slave_offset: u32) void {
-        _ = master_offset;
-        _ = slave_offset;
+    pub fn remap(master_offset: u8, slave_offset: u8) void {
+        // sending the initialization byte to a PIC makes it prepare for 3 more
+        // (I)nitialization (C)ommand (W)ords (ICW).
+        as.assembly_wrappers.x86_out(
+            master_command_port,
+            common_messages.pic_initialize,
+        );
+        serial.SerialPort.ioWait();
+        as.assembly_wrappers.x86_out(
+            slave_command_port,
+            common_messages.pic_initialize,
+        );
+        serial.SerialPort.ioWait();
+        // ICW 2: Offset
+        as.assembly_wrappers.x86_out(master_data_port, master_offset);
+        serial.SerialPort.ioWait();
+        as.assembly_wrappers.x86_out(slave_data_port, slave_offset);
+        serial.SerialPort.ioWait();
+        // ICW 3: Cascade setup
+        as.assembly_wrappers.x86_out(
+            master_data_port,
+            0b0000_0100, // slave at IRQ 2
+        );
+        serial.SerialPort.ioWait();
+        as.assembly_wrappers.x86_out(
+            slave_data_port,
+            0b0000_0010, // tell slave its cascade identity
+        );
+        serial.SerialPort.ioWait();
+        // ICW 4: 8086 mode
+        as.assembly_wrappers.x86_out(
+            master_data_port,
+            common_messages.initialize_8086_mode,
+        );
+        serial.SerialPort.ioWait();
+        as.assembly_wrappers.x86_out(
+            slave_data_port,
+            common_messages.initialize_8086_mode,
+        );
+        serial.SerialPort.ioWait();
+
+        as.assembly_wrappers.x86_out(master_data_port, common_messages.unmask);
+        serial.SerialPort.ioWait();
+        as.assembly_wrappers.x86_out(slave_data_port, common_messages.unmask);
+        serial.SerialPort.ioWait();
     }
 };
