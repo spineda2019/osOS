09ca65b2489d974980b8568cdf3dceb921e97208
spineda2019 <spineda.wpi.alum@gmail.com>
2025-05-06 22:26:57 -0400

    rename kmain to setup for arch specific setup

--- arch/x86/entry.zig
+++ arch/x86/entry.zig
@@ -14,7 +14,7 @@
 //! You should have received a copy of the GNU General Public License
 //! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-const setup: *const fn () noreturn = &@import("kmain.zig").setup;
+const setup: *const fn () noreturn = &@import("setup.zig").setup;
 
 const stack_top: [*]u8 = @extern([*]u8, .{ .name = "__stack_top" });
 
