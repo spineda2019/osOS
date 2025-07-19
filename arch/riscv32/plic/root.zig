ce000ced46aa31b8053ffea445adebd242eeff5a
spineda2019 <spineda.wpi.alum@gmail.com>
2025-07-19 17:30:50 -0400

    stub barebones plic module

---
+++ arch/riscv32/plic/root.zig
@@ -0,0 +1,18 @@
+//! root file for the plic driver module
+//! Copyright (C) 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/// TODO: do not hardcode; get from parsing DTB
+const plic_address: *volatile u8 = 0xc000000;
