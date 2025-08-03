75a297317d2a3ee06afb83a5d323b491ad53dc4e
spineda2019 <spineda.wpi.alum@gmail.com>
2025-08-03 18:44:41 -0400

    add generic handler for pic to be registered in idt; fix icw2

--- arch/x86/interrupts/pic.zig
+++ arch/x86/interrupts/pic.zig
@@ -19,15 +19,26 @@
 
 const as = @import("x86asm");
 const serial = @import("x86serial");
+const framebuffer = @import("x86framebuffer");
+const Self: type = @This();
+
+var framebuffer_handle: *framebuffer.FrameBuffer = undefined;
+var irq_offset: u8 = 0x20;
 
 const master_command_port: u16 = 0x0020;
 const master_data_port: u16 = 0x0021;
 const slave_command_port: u16 = 0x00A0;
 const slave_data_port: u16 = 0x00A1;
 
+pub const irq_number = enum(u8) {
+    timer = 0x20,
+    keyboard = 0x21,
+};
+
 /// simple namespace to avoid magic number use
 const common_messages = struct {
     const pic_acknowledge: u8 = 0x20;
+
     /// A "raw" initialization byte is technically 0x10, but ORing this with 0x1
     /// indicates we ae using "Cascading Mode", which gives us access to
     /// multiple chained PICs, so we'll just use 0x11 by default.
@@ -35,19 +46,29 @@ const common_messages = struct {
 
     const initialize_8086_mode: u8 = 0x01;
 
-    /// Enables all IRQs
-    const unmask: u8 = 0;
+    const masks = struct {
+        const keyboard: u8 = 0b0000_0010;
+        const timer: u8 = 0b0000_0001;
+        const unmask_all: u8 = 0;
 
-    /// informs the PIC (both the master and slave) which IRQ in the master
-    /// will be used to cascade IRQs from the slave from. This wires the
-    /// slave to the master via IRQ 2 (the 2nd 0-based bit is set).
-    const pic_cascade_mask: u8 = 0b0000_0100;
+        /// informs the master PIC which IRQ
+        /// will be used to cascade IRQs from the slave. This wires the
+        /// slave to the master via IRQ 2 (the 2nd 0-based bit is set).
+        const slave_wire_on_master: u8 = 0b0000_0100;
+
+        /// informs the slave PIC which IRQ
+        /// will be used to cascade IRQs to the master. This wires the
+        /// slave to the master via IRQ 2 (the number 2 in binary).
+        const slave_cascade_irq: u8 = 0b0000_0010;
+    };
 };
 
 /// Initialize the PIC master and slave with a predefined offset into the
 /// IDT (or else the default IRQ nums will be 0-7 which conflict wity x86
 /// CPU exceptions)
-pub fn init(master_offset: u8, slave_offset: u8) void {
+pub fn init(fb_handle: *framebuffer.FrameBuffer) void {
+    // TODO: log warn if no pic found. Right ow we assume it exists.
+
     // sending the initialization byte to a PIC makes it prepare for 3 more
     // (I)nitialization (C)ommand (W)ords (ICW).
 
@@ -65,22 +86,22 @@ pub fn init(master_offset: u8, slave_offset: u8) void {
     serial.SerialPort.ioWait();
 
     // ************************* ICW 2: Offset ************************** //
-    as.assembly_wrappers.x86_out(master_data_port, master_offset);
+    as.assembly_wrappers.x86_out(master_data_port, irq_offset);
     serial.SerialPort.ioWait();
 
-    as.assembly_wrappers.x86_out(slave_data_port, slave_offset);
+    as.assembly_wrappers.x86_out(slave_data_port, irq_offset + 8);
     serial.SerialPort.ioWait();
 
     // ************** ICW 3: Cascade (Master<-Slave) Setup ************** //
     as.assembly_wrappers.x86_out(
         master_data_port,
-        common_messages.pic_cascade_mask,
+        common_messages.masks.slave_wire_on_master,
     );
     serial.SerialPort.ioWait();
 
     as.assembly_wrappers.x86_out(
         slave_data_port,
-        common_messages.pic_cascade_mask, // tell slave its cascade identity
+        common_messages.masks.slave_cascade_irq,
     );
     serial.SerialPort.ioWait();
 
@@ -97,10 +118,16 @@ pub fn init(master_offset: u8, slave_offset: u8) void {
     );
     serial.SerialPort.ioWait();
 
-    as.assembly_wrappers.x86_out(master_data_port, common_messages.unmask);
+    as.assembly_wrappers.x86_out(
+        master_data_port,
+        comptime common_messages.masks.keyboard | common_messages.masks.timer,
+    );
     serial.SerialPort.ioWait();
-    as.assembly_wrappers.x86_out(slave_data_port, common_messages.unmask);
+    // as.assembly_wrappers.x86_out(slave_data_port, common_messages.unmask);
+    as.assembly_wrappers.x86_out(slave_data_port, 0b1111_1111);
     serial.SerialPort.ioWait();
+
+    framebuffer_handle = fb_handle;
 }
 
 /// Send acknowledgement to the PIC chip that sent an interrupt request. If
@@ -121,5 +148,19 @@ fn sendAcknowledgement(interrupt_request: u8) void {
     );
 }
 
-/// Registered in IDT in idt.zig
-pub fn handleIRQ() callconv(.naked) void {}
+fn handleKeyboardIRQ() void {}
+
+fn handleTimerIRQ() void {
+    framebuffer_handle.write(".");
+}
+
+pub export fn handleGenericPicIrq(irq_with_offset: u8) callconv(.c) void {
+    as.assembly_wrappers.disable_x86_interrupts();
+    const target: irq_number = @enumFromInt(irq_with_offset);
+    switch (target) {
+        .keyboard => handleKeyboardIRQ(),
+        .timer => handleTimerIRQ(),
+    }
+    sendAcknowledgement(irq_with_offset - irq_offset);
+    as.assembly_wrappers.enable_x86_interrupts();
+}
