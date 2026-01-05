ab1053459e4fdb1e7a49f17babb442a997b18d61
spineda2019 <spineda.wpi.alum@gmail.com>
2026-01-05 18:21:44 -0500

    more aggressive inlining

--- format/format.zig
+++ format/format.zig
@@ -85,102 +85,90 @@ test StringFromInt {
         }
     };
 
-    const fit_in_u8: []const Test.Config(u8) = &.{
+    const tests = comptime .{
+        // >= u8
         .{
-            .number = 0,
-            .base = 10,
-            .expected_string = "0",
+            [4]type{ u8, u16, u32, usize },
+            [3]Test.Config(u8){
+                .{
+                    .number = 0,
+                    .base = 10,
+                    .expected_string = "0",
+                },
+                .{
+                    .number = 1,
+                    .base = 10,
+                    .expected_string = "1",
+                },
+                .{
+                    .number = 255,
+                    .base = 10,
+                    .expected_string = "255",
+                },
+            },
         },
+        // >= u16
         .{
-            .number = 1,
-            .base = 10,
-            .expected_string = "1",
+            [3]type{ u16, u32, usize },
+            [2]Test.Config(u16){
+                .{
+                    .number = 1234,
+                    .base = 10,
+                    .expected_string = "1234",
+                },
+                .{
+                    .number = 12345,
+                    .base = 10,
+                    .expected_string = "12345",
+                },
+            },
         },
+        // >= u32
         .{
-            .number = 255,
-            .base = 10,
-            .expected_string = "255",
+            [2]type{ u32, usize },
+            [5]Test.Config(u32){
+                .{
+                    .number = 123456,
+                    .base = 10,
+                    .expected_string = "123456",
+                },
+                .{
+                    .number = 1234567,
+                    .base = 10,
+                    .expected_string = "1234567",
+                },
+                .{
+                    .number = 12345678,
+                    .base = 10,
+                    .expected_string = "12345678",
+                },
+                .{
+                    .number = 123456789,
+                    .base = 10,
+                    .expected_string = "123456789",
+                },
+                .{
+                    .number = 1234567890,
+                    .base = 10,
+                    .expected_string = "1234567890",
+                },
+            },
         },
     };
 
-    inline for ([4]type{ u8, u16, u32, usize }) |ty| {
-        for (fit_in_u8) |test_instance| {
-            const toTest: StringFromInt(ty) = .init(.{
-                .number = test_instance.number, // type upcast occurs
-                .base = test_instance.base,
-            });
-            try std.testing.expect(std.mem.eql(
-                u8,
-                test_instance.expected_string,
-                toTest.getStr(),
-            ));
-        }
-    }
-
-    const fit_in_u16: []const Test.Config(u16) = &.{
-        .{
-            .number = 1234,
-            .base = 10,
-            .expected_string = "1234",
-        },
-        .{
-            .number = 12345,
-            .base = 10,
-            .expected_string = "12345",
-        },
-    };
-    inline for ([3]type{ u16, u32, usize }) |ty| {
-        for (fit_in_u16) |test_instance| {
-            const toTest: StringFromInt(ty) = .init(.{
-                .number = test_instance.number,
-                .base = test_instance.base,
-            });
-            try std.testing.expect(std.mem.eql(
-                u8,
-                test_instance.expected_string,
-                toTest.getStr(),
-            ));
-        }
-    }
-
-    const fit_in_u32: []const Test.Config(u32) = &.{
-        .{
-            .number = 123456,
-            .base = 10,
-            .expected_string = "123456",
-        },
-        .{
-            .number = 1234567,
-            .base = 10,
-            .expected_string = "1234567",
-        },
-        .{
-            .number = 12345678,
-            .base = 10,
-            .expected_string = "12345678",
-        },
-        .{
-            .number = 123456789,
-            .base = 10,
-            .expected_string = "123456789",
-        },
-        .{
-            .number = 1234567890,
-            .base = 10,
-            .expected_string = "1234567890",
-        },
-    };
-    inline for ([2]type{ u32, usize }) |ty| {
-        for (fit_in_u32) |test_instance| {
-            const toTest: StringFromInt(ty) = .init(.{
-                .number = test_instance.number,
-                .base = test_instance.base,
-            });
-            try std.testing.expect(std.mem.eql(
-                u8,
-                test_instance.expected_string,
-                toTest.getStr(),
-            ));
+    inline for (tests) |test_config| {
+        inline for (test_config.@"0") |T| {
+            inline for (test_config.@"1") |test_instance| {
+                const toTest: StringFromInt(T) = .init(.{
+                    .number = test_instance.number, // type upcast occurs
+                    .base = test_instance.base,
+                });
+                try std.testing.expect(std.mem.eql(
+                    u8,
+                    test_instance.expected_string,
+                    toTest.getStr(),
+                ));
+            }
         }
     }
 }
