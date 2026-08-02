f83144398c00d2dbcf3de2ff996081816fdd509c
spineda2019 <spineda.wpi.alum@gmail.com>
2026-08-01 23:31:12 -0400

    log to serial

--- kmain/kmain.zig
+++ kmain/kmain.zig
@@ -28,35 +28,50 @@ const testoptions = @import("testoptions");
 /// and arbitrary illegal instruction for testing, etc) will be provided via
 /// functions in `ct_hal`. Might be an escape hatch, but I like the abstraction.
 pub fn kmain(rt_hal: oshal.RtHAL, comptime ct_hal: oshal.CtHal) noreturn {
-    var terminal = rt_hal.terminal;
-    var serial = rt_hal.serial_io;
+    const Logger = struct {
+        serial: osformat.IWriter,
+        terminal: osformat.IWriter,
+
+        const Logger = @This();
+        fn writef(self: *Logger, comptime format: []const u8, args: anytype) void {
+            self.serial.writef(format, args);
+            self.terminal.writef(format, args);
+        }
+        fn flush(self: *Logger) void {
+            self.serial.flush();
+            self.terminal.flush();
+        }
+    };
+    var logger: Logger = .{
+        .serial = rt_hal.serial_io,
+        .terminal = rt_hal.terminal,
+    };
 
     for (0..12) |_| {
-        terminal.writef("Foo " ** 20, .{});
-        terminal.writef("Bar " ** 20, .{});
-        terminal.writef("Baz " ** 20, .{});
+        logger.writef("Foo " ** 20, .{});
+        logger.writef("Bar " ** 20, .{});
+        logger.writef("Baz " ** 20, .{});
     }
 
-    terminal.writef("Hey there! We succesfully passed the HAL to kmain\r\n", .{});
-    terminal.writef("Testing writeLine...\r\n", .{});
-    terminal.writef("Hi there from a new line!\r\n", .{});
-    terminal.writef("Hi there from a new line again!\r\n", .{});
+    logger.writef("Hey there! We succesfully passed the HAL to kmain\r\n", .{});
+    logger.writef("Testing writeLine...\r\n", .{});
+    logger.writef("Hi there from a new line!\r\n", .{});
+    logger.writef("Hi there from a new line again!\r\n", .{});
 
     if (testoptions.test_panic) {
-        terminal.writef("Testing Panic\r\n", .{});
+        logger.writef("Testing Panic\r\n", .{});
         @panic("Testing Panic");
     }
 
     if (testoptions.test_ill) {
-        terminal.writef(
+        logger.writef(
             "Purposefully performing an illegal instruction...\r\n",
             .{},
         );
         ct_hal.assembly_wrappers.illegal_instruction();
     }
 
-    terminal.flush();
-    serial.flush();
+    logger.flush();
 
     var process_pool: process.ProcessTable(8) = .init();
     _ = &process_pool;
